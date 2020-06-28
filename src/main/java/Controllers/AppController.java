27a9342466c74d3c67300b98a1b00e3529f2083e
Hasara <hasarasamson97@gmail.com>
2020-06-28 10:24:36 +0530

    Controller class

--- src/main/java/Controllers/AppController.java
+++ src/main/java/Controllers/AppController.java
@@ -1,13 +1,26 @@
 package Controllers;
 
 import Services.ProductService;
+import com.tutorial.demo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
 
     @Autowired
     private ProductService service;
+
+    @RequestMapping("/")
+    public String viewHomePage(Model model){
+        List<Product> listProducts = service.listAll();
+        model.addAttribute("listProducts", listProducts);
+
+        return "index";
+    }
 }
 
