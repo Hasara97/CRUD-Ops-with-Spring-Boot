9ff0945f19bf339a97cfee47636bb9e34ea4082e
Hasara <hasarasamson97@gmail.com>
2020-06-28 10:47:25 +0530

    CRUD

--- src/main/java/Controllers/AppController.java
+++ src/main/java/Controllers/AppController.java
@@ -5,7 +5,11 @@ import com.tutorial.demo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -22,5 +26,38 @@ public class AppController {
 
         return "index";
     }
+
+    @RequestMapping("/new")
+    public String showNewProductPage(Model model){
+        Product product = new Product();
+        model.addAttribute("Product", product);
+
+        return "new_product";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product){
+        service.saveProduct(product);
+
+        return  "redirect:/";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductPage(@PathVariable(name="id") int id){
+        ModelAndView mav = new ModelAndView();
+        Product product = service.get(id);
+        mav.addObject("product", product);
+
+        return mav;
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable(name = "id") int id){
+        service.delete(id);
+        return  "redirect:/";
+
+    }
+
+
 }
 
